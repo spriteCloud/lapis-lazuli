08a5ea8627ff443cdaa2a3cd118aa474a66071a9
Jens Finkhaeuser <jens@finkhaeuser.de>
2013-12-11 11:01:45 +0100

    CLI implementation & documentation

--- lib/spritecuke/cli.rb
+++ lib/spritecuke/cli.rb
@@ -1,13 +1,20 @@
+#################################################################################
+# Copyright 2013,2014 spriteCloud B.V. All rights reserved.
+# Author: "Jens Finkhaeuser" <jens@spritecloud.com>
 require 'thor'
 
 require 'spritecuke/generators/cucumber'
 
 module Spritecuke
   class CLI < Thor
+    class_option :verbose, :aliases => "-v", :type => :boolean, :default => false, :desc => "Be verbose."
 
-    desc "create", "Creates a cucumber project with some common step definitions."
-    def create(path, name)
-      Spritecuke::Generators::Cucumber.start([path, name])
-    end
+    long_desc <<-LONGDESC
+      Creates a cucumber test script directory pre-seeded with common step
+      definitions, environment and configuratin support, and all the bells
+      and whistles of a proper spriteCloud test setup.
+    LONGDESC
+    option :force, :aliases => "-f", :type => :boolean, :default => false, :desc => "Always overwrite existing files."
+    register(Spritecuke::Generators::Cucumber, "create", "create PROJECT", "Creates a cucumber project with some common step definitions.")
   end
 end
