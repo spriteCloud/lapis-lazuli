2a8c7f7499dbdd8e3d4be874e66177179cd33e8c
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-12-10 11:48:08 +0100

    Add command for documenting currently supported configuration options.

--- lib/lapis_lazuli/cli.rb
+++ lib/lapis_lazuli/cli.rb
@@ -8,6 +8,7 @@
 require 'thor'
 
 require 'lapis_lazuli/generators/cucumber'
+require 'lapis_lazuli/options'
 
 module LapisLazuli
   class CLI < Thor
@@ -28,6 +29,65 @@ module LapisLazuli
     option :branch, :aliases => "-b", :type => :string, :default => nil, :desc => "Specify the github branch of Lapis Lazuli a created/updated project is to use."
     register(LapisLazuli::Generators::Cucumber, "create", "create PROJECT", "Creates a cucumber project with some common step definitions.")
     register(LapisLazuli::Generators::Cucumber, "update", "update PROJECT", "Updates a project. Alias for 'create'.")
+
+    desc "config", "List available configuration options."
+    def config 
+
+      STDOUT.write <<-INTRO
+LapisLazuli searches for configuration files in the `config' subdirectory of
+the current working directory, taking the stated test environment into
+consideration.
+
+Example:
+  ENV['TEST_ENV'] = 'production'
+  load_config("config/config.yml")
+
+Will try to load the following files, in order:
+  - config/config-production.yml
+  - config/config-debug.yml
+  - config/config-test.yml
+  - config/config-local.yml
+  - config/config.yml
+
+The first configuration file in the list that is found will be loaded, and its
+contents become available via the configuraiton functions.
+
+Supported configuration formats and file name extensions are:
+  .yml  - YAML file
+  .json - JSON file
+
+In addition to environment-specific configuration files, LapisLazuli supports
+the concept of test environments within a single file, where environments are
+just top-level keys, e.g.:
+
+  production:
+    - config for the production environment
+
+  development:
+    - config for the development environment
+
+The configuration files can contain any configuration options, but a few are
+interpreted by LapisLazuli. Note that instead of specifying these supported
+options in the configuraiton file, you may also provide them in the environment
+(convert option name to upper case).
+
+INTRO
+      STDOUT.flush
+
+      LapisLazuli::CONFIG_OPTIONS.each do |option, value|
+        printf "%22s\n", option
+
+        display_default = value[0]
+        if display_default.nil?
+          display_default = "No default."
+        else
+          display_default = "Defaults to '#{display_default}'."
+        end
+        printf "              #{display_default}\n"
+        printf "              #{value[1]}\n\n"
+
+      end
+    end
   end
 
 
---
+++ lib/lapis_lazuli/options.rb
@@ -0,0 +1,21 @@
+#
+# LapisLazuli
+# https://github.com/spriteCloud/lapis-lazuli
+#
+# Copyright (c) 2013-2014 spriteCloud B.V. and other LapisLazuli contributors.
+# All rights reserved.
+#
+module LapisLazuli
+  ##
+  # Configuration options and their default values
+  CONFIG_OPTIONS = {
+    "close_browser_after" => ["feature", "Close the browser after every scenario, feature, etc. Possible values are 'feature', 'scenario' and 'never'."],
+    "error_strings" => [nil, "List of strings that indicate errors when detected on a web page."],
+    "default_env" => [nil, "Indicates which environment specific configuration to load when no test environment is provided explicitly."],
+    "email_domain" => ["google.com", "FIXME"],
+    "screenshot_on_failure" => [true, "Toggle whether failed scenarios should result in a screenshot being taken automatically."],
+    "screenshot_dir" => ["FIXME", "Location prefix for the screenshot path."],
+    "screenshot_scheme" => ["old", "Naming scheme for screenshots. Possible values are 'old' and 'new'. This option will be deprecated in the near future."],
+    "breakpoint_on_error" => [false, "If the error() function is used to create errors, should the debugger be started?"],
+  }
+end # module LapisLazuli
