eec5e9eb1fc0479086c8b5df13ac31948caacb5d
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-12-11 20:10:22 +0100

    Make it possible to use World(LapisLazuli) in cucumber now

--- lib/lapis_lazuli.rb
+++ lib/lapis_lazuli.rb
@@ -7,5 +7,28 @@
 #
 
 require "lapis_lazuli/version"
-require "lapis_lazuli/lapis_lazuli"
+require "lapis_lazuli/world"
 
+module LapisLazuli
+  ##
+  # Explicitly store the configuration file name
+  def self.config_file=(name)
+    @config_filename = name
+  end
+
+  def self.config_file
+    @config_filename
+  end
+
+  ##
+  # Pass just about everything on to the World class
+  def respond_to?(meth)
+    p LapisLazuli.instance_methods
+    p "respond to ? #{meth}"
+    return World.instance.respond_to? meth
+  end
+
+  def method_missing(meth, *args, &block)
+    return World.instance.send(meth.to_s, *args, &block)
+  end
+end # module LapisLazuli
