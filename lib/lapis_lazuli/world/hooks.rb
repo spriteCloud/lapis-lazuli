7c5587557b344312ebee62e5803bb755d1e31f45
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-12-16 14:21:59 +0100

    Make WorldModule::Hooks resilient to other WorldModules being loaded or not.

--- lib/lapis_lazuli/world/hooks.rb
+++ lib/lapis_lazuli/world/hooks.rb
@@ -10,37 +10,52 @@ module LapisLazuli
 module WorldModule
   ##
   # Module with cucumber hooks
+  #
+  # The module is special in that it does not include other modules. Instead it
+  # always tests whether World responds to a function before calling it.
   module Hooks
     ##
     # Hook invoked in BeforeScenario
     def before_scenario_hook(cuke_scenario)
       # Update the scenario informaton
-      scenario.running = true
-      scenario.update(cuke_scenario)
+      if respond_to? :scenario
+        scenario.running = true
+        scenario.update(cuke_scenario)
+      end
+
       # Show the name
-      log.info("Starting Scenario: #{scenario.id}")
+      if respond_to? :log
+        log.info("Starting Scenario: #{scenario.id}")
+      end
     end
 
     ##
     # Hook invoked in AfterScenario
     def after_scenario_hook(cuke_scenario)
       # The current scenario has finished
-      scenario.running = false
+      if respond_to? :scenario
+        scenario.running = false
+      end
 
       # Sleep if needed
-      if has_env_or_config?("step_pause_time")
+      if respond_to? :config and has_env_or_config?("step_pause_time")
         sleep env_or_config("step_pause_time")
       end
 
       # Did we fail?
-      if (cuke_scenario.failed? or (scenario.check_browser_errors and browser.has_error?))
-        # Take a screenshot if needed
-        if has_env_or_config?('screenshot_on_failure')
-          fileloc = browser.take_screenshot()
+      if respond_to? :scenario and respond_to? :browser and respond_to? :config
+        if (cuke_scenario.failed? or (scenario.check_browser_errors and browser.has_error?))
+          # Take a screenshot if needed
+          if has_env_or_config?('screenshot_on_failure')
+            fileloc = browser.take_screenshot()
+          end
         end
       end
+
       # Close browser if needed
-      browser.close_after_scenario(cuke_scenario)
+      if respond_to? :browser
+        browser.close_after_scenario(cuke_scenario)
+      end
     end
   end # module Hooks
 end # module WorldModule
