7b320d82d0a29c57a85db1c1edab8e63d8b48f70
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-12-16 14:23:11 +0100

    Fix scope resolution in WorldModule::Variable

--- lib/lapis_lazuli/world/variable.rb
+++ lib/lapis_lazuli/world/variable.rb
@@ -6,46 +6,100 @@
 # All rights reserved.
 #
 
+require "securerandom"
+
+require "lapis_lazuli/scenario"
+
+require "lapis_lazuli/world/config"
+
 module LapisLazuli
 module WorldModule
   ##
   # Module for variable replacement
   #
-  # Uses the following managed by World:
-  #   @time
-  #   @scenario
-  #   @uuid
+  # Manages the following:
+  #   @scenario   - for per-scenario variables
+  #   @uuid       - for the entire test run
+  #   @time       - for the entire test run
+  #   @storage    - for the entire test run
   module Variable
+    include LapisLazuli::WorldModule::Config
+
+    ##
+    # Scenario "singleton"
+    def scenario
+      if @scenario.nil?
+        @scenario = Scenario.new
+      end
+      return @scenario
+    end
+
+    ##
+    # Time "singleton"
+    def time
+      if @time.nil?
+        time = Time.now
+        @time = {
+          :timestamp => time.strftime('%y%m%d_%H%M%S'),
+          :epoch => time.to_i.to_s
+        }
+      end
+      return @time
+    end
+
+    ##
+    # UUID "singleton"
+    def uuid
+      if @uuid.nil?
+        @uuid = SecureRandom.hex
+      end
+      return @uuid
+    end
+
+    ##
+    # Storage "singleton"
+    def storage
+      if @storage.nil?
+        @storage = Storage.new
+        @storage.set("time", time)
+        @storage.set("uuid", uuid)
+      end
+      return @storage
+    end
+
+
     ##
     # Update the variable with timestamps
     def variable(string)
+      init
+
       email_domain = "spriteymail.net"
-      if self.has_env_or_config?("email_domain")
-        email_domain = self.env_or_config("email_domain")
+      if has_env_or_config?("email_domain")
+        email_domain = env_or_config("email_domain")
       end
       random_uuid = SecureRandom.hex
       string % {
-        :epoch => @time[:epoch],
-        :timestamp => @time[:timestamp],
-        :uuid => @uuid,
-        :email => "test_#{@uuid}@#{email_domain}",
-        :scenario_id => @scenario.id,
-        :scenario_epoch => @scenario.time[:epoch],
-        :scenario_timestamp => @scenario.time[:timestamp],
-        :scenario_email => "test_#{@uuid}_scenario_#{@scenario.uuid}@#{email_domain}",
-        :scenario_uuid => @scenario.uuid,
+        :epoch => time[:epoch],
+        :timestamp => time[:timestamp],
+        :uuid => uuid,
+        :email => "test_#{uuid}@#{email_domain}",
+        :scenario_id => scenario.id,
+        :scenario_epoch => scenario.time[:epoch],
+        :scenario_timestamp => scenario.time[:timestamp],
+        :scenario_email => "test_#{uuid}_scenario_#{scenario.uuid}@#{email_domain}",
+        :scenario_uuid => scenario.uuid,
         :random => rand(9999),
         :random_small => rand(99),
         :random_lange => rand(999999),
         :random_uuid => random_uuid,
-        :random_email => "test_#{@uuid}_random_#{random_uuid}@#{email_domain}"
+        :random_email => "test_#{uuid}_random_#{random_uuid}@#{email_domain}"
       }
     end
 
     ##
     # Same as variable, but modify the string.
     def variable!(string)
-      string.replace(self.variable(string))
+      string.replace(variable(string))
     end
 
 
