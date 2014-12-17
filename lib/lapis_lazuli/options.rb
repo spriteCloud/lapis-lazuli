2f875ff617705c06fcae79aa39c4f3863db2f44a
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-12-17 14:54:29 +0100

    Fix documentation

--- lib/lapis_lazuli/options.rb
+++ lib/lapis_lazuli/options.rb
@@ -14,7 +14,7 @@ module LapisLazuli
     "default_env" => [nil, "Indicates which environment specific configuration to load when no test environment is provided explicitly."],
     "test_env" => [nil, "Indicates which environment specific configuration to load in this test run."],
     "browser" => ['firefox', "Indicates the browser in which to run tests. Possible values are 'firefox', 'chrome', 'safari', 'ie', 'ios'."],
-    "email_domain" => ["google.com", "FIXME"],
+    "email_domain" => ["google.com", "The domain name used when generating email addresses. See the `placeholders` command for more information."],
     "screenshot_on_failure" => [true, "Toggle whether failed scenarios should result in a screenshot being taken automatically."],
     "screenshot_dir" => [".#{File::SEPARATOR}screenshots", "Location prefix for the screenshot path."],
     "screenshot_scheme" => ["old", "Naming scheme for screenshots. Possible values are 'old' and 'new'. This option will be deprecated in the near future, and only the new scheme will be supported."],
