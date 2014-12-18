f26b132d252ad22d99e5e71e6e3a7c5d0fe26887
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-12-18 15:14:33 +0100

    Update template; this really should have happened before 0.5.0

--- lib/lapis_lazuli/generators/cucumber/template/features/support/transition.rb
+++ lib/lapis_lazuli/generators/cucumber/template/features/support/transition.rb
@@ -2,12 +2,11 @@
 # All function in this file SHOULD not be used.
 # There only included for backwards compatibility
 #
-ll = LapisLazuli::LapisLazuli.instance
 
 ##
 # Creating a link for easy debugging afterwards
 def create_link(name, url)
-  ll.log.info("[DEPRECATED] [feature/support/transition.rb] create_link")
+  log.info("[DEPRECATED] [feature/support/transition.rb] create_link")
   #Lets just send the url without the parameters to prevent html display problems
   "<a href='#{url}' target='_blank'>#{name}</a>"
 end
